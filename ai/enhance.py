dbbe4f0a15312291696df876bdbd796505331d4d
davidzhh <1513699928@qq.com>
2025-06-14 21:12:25 +0800

    update

--- ai/enhance.py
+++ ai/enhance.py
@@ -8,15 +8,17 @@ import argparse
 import langchain_core.exceptions
 from langchain_openai import ChatOpenAI
 from langchain.prompts import (
-  ChatPromptTemplate,
-  SystemMessagePromptTemplate,
-  HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
 )
 from structure import Structure
+
 if os.path.exists('.env'):
     dotenv.load_dotenv()
 template = open("template.txt", "r").read()
 system = open("system.txt", "r").read()
+
 
 def parse_args():
     """解析命令行参数"""
@@ -24,9 +26,10 @@ def parse_args():
     parser.add_argument("--data", type=str, required=True, help="jsonline data file")
     return parser.parse_args()
 
+
 def main():
     args = parse_args()
-    model_name = os.environ.get("MODEL_NAME", 'Qwen/Qwen3-235B-A22B')
+    model_name = os.environ.get("MODEL_NAME", 'Qwen/Qwen3-8B')
     language = os.environ.get("LANGUAGE", 'Chinese')
 
     data = []
@@ -47,33 +50,30 @@ def main():
 
     llm = ChatOpenAI(model=model_name).with_structured_output(Structure, method="function_calling")
     print('Connect to:', model_name, file=sys.stderr)
-    prompt_template = ChatPromptTemplate.from_messages([
-        SystemMessagePromptTemplate.from_template(system),
-        HumanMessagePromptTemplate.from_template(template=template)
-    ])
+    prompt_template = ChatPromptTemplate.from_messages(
+        [SystemMessagePromptTemplate.from_template(system), HumanMessagePromptTemplate.from_template(template=template)]
+    )
 
     chain = prompt_template | llm
 
     for idx, d in enumerate(data):
         try:
-            response: Structure = chain.invoke({
-                "language": language,
-                "content": d['summary']
-            })
+            response: Structure = chain.invoke({"language": language, "content": d['summary']})
             d['AI'] = response.model_dump()
         except langchain_core.exceptions.OutputParserException as e:
             print(f"{d['id']} has an error: {e}", file=sys.stderr)
             d['AI'] = {
-                 "tldr": "Error",
-                 "motivation": "Error",
-                 "method": "Error",
-                 "result": "Error",
-                 "conclusion": "Error"
+                "tldr": "Error",
+                "motivation": "Error",
+                "method": "Error",
+                "result": "Error",
+                "conclusion": "Error",
             }
         with open(args.data.replace('.jsonl', f'_AI_enhanced_{language}.jsonl'), "a") as f:
             f.write(json.dumps(d) + "\n")
 
         print(f"Finished {idx+1}/{len(data)}", file=sys.stderr)
+
 
 if __name__ == "__main__":
     main()
